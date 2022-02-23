74b036ec5f63add3a18577e789e6e3222327a26a
anshushrivastava67 <78591207+anshushrivastava67@users.noreply.github.com>
2022-02-23 12:06:12 +0530

    Update test.c

--- lab question/Q5} Decimal to binary conversion./test.c
+++ lab question/Q5} Decimal to binary conversion./test.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+void convert(int n)
+{
+    int i,d,j,s,a[15];
+    printf("Your number is converted in binary:\n");
+    for(i=0;n>=1;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        printf("%d",a[j]);
+    }
+    
+}
 
 int main()
 {
-    int s,i,n,a[100],d=0;
+    int s,i,n,a,d=0;
+    
     printf("Enter the Decimal positive number:");
     scanf("%d",&n);
-    for(i=0;n!='\0';i++)
-    {
-        s=n%2;
-        d=s*pow(10,i)+d;
-        n=n/2;
-        printf("%d",d);
-    }
-
+    convert(n);
+    
+  
     return 0;
 }
+
 
 
